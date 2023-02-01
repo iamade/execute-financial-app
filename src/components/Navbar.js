70d530d5f74bbefb641fb3830963b46a4b8f1db0
Adesegun Koiki <kamar3deen@gmail.com>
2023-02-01 18:22:48 +0100

    fisrt commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import logo from './images/logo.png'
+
+import './Navbar.css'
+
+function Navbar() {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+  return (
+    <div className='header'>
+        <nav className='navbar'>
+            <a href='/' className='logo'>
+                <img src={logo} alt='logo' />
+            </a>
+            <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes size={30} style={{color: '#ffffff'}} /> ) 
+                : (<FaBars size={30} style={{color: '#ffffff'}}/>) }
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <a href='/'>Home</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='/'>About</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='/'>Testimonials</a>
+                </li>
+                <li className='nav-item'>
+                    <a href='/'>Demo</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
